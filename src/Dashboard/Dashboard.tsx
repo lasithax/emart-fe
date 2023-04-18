8fb137668198f514cb1720c8f5a0e86f203cdf3f
lasitha udayanga <lasitha@altrium.io>
2023-04-18 23:11:24 +0530

    installed antd created inital routes & nav header

---
+++ src/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AppSider from '../Navigation/AppSider'
+
+function Dashboard() {
+  return (
+    <AppSider/>
+  )
+}
+
+export default Dashboard
---
+++ src/Navigation/AppSider.jsx
@@ -0,0 +1,46 @@
+import {
+  ShoppingCartOutlined,
+  HomeOutlined,
+  ContactsOutlined,
+} from "@ant-design/icons";
+import { Menu } from "antd";
+import { useState } from "react";
+
+import './appSider.css';
+
+const items = [
+  {
+    label: "Home",
+    key: "home",
+    icon: <HomeOutlined />,
+  },
+  {
+    label: "Shop",
+    key: "shop",
+    icon: <ShoppingCartOutlined />,
+  },
+  {
+    label: "Contact",
+    key: "contact",
+    icon: <ContactsOutlined />,
+  },
+];
+
+const AppSider = () => {
+  const [current, setCurrent] = useState("home");
+  const onClick = (e) => {
+    setCurrent(e.key);
+  };
+  return (
+    <div className="navigationBar">
+      <Menu
+        onClick={onClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+        items={items}
+      />
+    </div>
+  );
+};
+
+export default AppSider;
